2ca980285623d14fe8806aaa9f04ced88b332c6b
jc <jc@jc>
2018-05-21 09:35:53 -0400

    [consensus] enforce max tx size check

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -19,8 +19,8 @@ static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 4000000;
 static const unsigned int MAX_BLOCK_WEIGHT = 8000000;
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const int64_t MAX_BLOCK_SIGOPS_COST = 200000;
-/** The maximum size of a transaction (network rule) */
-static const unsigned int MAX_TX_WEIGHT = 400000;
+/** The maximum size of a transaction excluding witnesses (network rule that is not particularly relevant any longer so not applied to witnesses) */
+static const unsigned int MAX_TX_SIZE = 100000;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
 
